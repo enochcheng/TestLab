2ba1be9ee7a108f934959b2870f6f2546a739fef
Yan Nok Cheng <yannok.cheng@gm.com>
2017-08-01 10:50:20 -0400

    Reorder project and added Android.mk

---
+++ Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# We only want this apk build for tests.
+LOCAL_MODULE_TAGS := debug optional tests
+
+# Include all test java files.
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src/main)
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-test
+
+LOCAL_MODULE := android-xml-run-listener
+LOCAL_CERTIFICATE := platform
+LOCAL_JACK_ENABLED := disabled
+LOCAL_DEX_PREOPT := false
+
+include $(BUILD_JAVA_LIBRARY)
